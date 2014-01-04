e51315304e124a459dee5d6ed32fae548aa3544c
Christoph Keller <gri@not-censored.com>
2014-01-04 15:56:31 +0100

    Added missing constants.h

---
+++ src/yappari-application/Whatsapp/constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+class Constants
+{
+public:
+    Constants();
+    
+    static const char CONNECT_BINARY_DOMAIN_ASCII[];
+    static const char BASE_URL[];
+    static const char URL_QUERY_STATUS[];
+};
+
+#endif // CONSTANTS_H
